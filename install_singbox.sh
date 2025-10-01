32f5d1804b0ea4a547fac84cd50c48adb8423451
hiozjl <45966885+hiozjl@users.noreply.github.com>
2025-10-01 12:26:04 +0800

    Update install_singbox.sh

--- install_singbox.sh
+++ install_singbox.sh
@@ -26,7 +26,7 @@ do
     case $opt in
         "安装sing-box")
             echo "正在安装sing-box..."
-            bash <(curl -fsSL https://sing-box.app/install.sh | sh -s -- --version 1.11.15)
+            curl -fsSL https://sing-box.app/install.sh | sh -s -- --version 1.11.15
             echo "安装sing-box成功！"
             ;;
         "配置reality-brutal")
